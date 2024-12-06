c6c53d2b4c80aec20f39ee3b6368c5ed5d553906
Jesús Flórez <71992811+dvchinx@users.noreply.github.com>
2024-12-05 19:44:46 -0500

    First Commit
    Upload code

---
+++ src/main/java/com/dvchinx/pizza/persistence/repository/PizzaPagSortRepository.java
@@ -0,0 +1,10 @@
+package com.dvchinx.pizza.persistence.repository;
+
+import com.dvchinx.pizza.persistence.entity.PizzaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
+
+public interface PizzaPagSortRepository extends ListPagingAndSortingRepository<PizzaEntity, Integer> {
+    Page<PizzaEntity> findByAvailableTrue(Pageable pageable);
+}
